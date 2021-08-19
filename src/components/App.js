2d9082e072c728d6f682c818ce880b2837a5bde0
rehith3 <rehith.k@gmail.com>
2021-08-19 20:10:27 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+
+function gen(){
+    const cd=new Date();
+    const hr=cd.getHours();
+    const min=cd.getMinutes();
+    const sec=cd.getSeconds();
+    cd.setHours(9)
+     
+    return cd.toLocaleTimeString()+" "+(hr>=12?'PM':'AM');
+}
+
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            time:gen(),
+        }
+    }
+
+    componentDidMount(){
+        this.intervalId=setInterval(()=>{
+            this.setState({time:gen()})
+        },1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalId)
+    }
+
+
     render() {
 
         return(
-            <>
-               
-            </>
+               <div className="Clock">
+                    <h3 id="time">{this.state.time}</h3>
+               </div>
         )
     }
 }
